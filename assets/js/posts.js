b087ad4c467cf0b856f3da953453606b216c4b2e
Patricio Monpelat <patricio.monpelat@gmail.com>
2025-10-21 14:10:03 -0300

    .

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -1,52 +1,57 @@
 const posts = [
-    {
-      title: "¿Cómo ganar nuevos clientes a través del SEO?",
-      image_url: "../assets/img/blog/como-ganar-nuevos-clientes-a-traves-del-seo.jpg",
-      href:"como-ganar-nuevos-clientes-a-traves-del-seo",
-      alt:"como ganar nuevos clientes a traves del seo"
-    },
-    {
-      title: "Qué son las palabras clave y cómo usarlas",
-      image_url: "../assets/img/blog/que-son-las-palabras-clave-y-como-usarlas.jpg",
-      href:"que-son-las-palabras-clave-y-como-usarlas",
-      alt:"que son las palabras clave y como usarlas"
-    },
-    {
-      title: "¿Cómo hacer publicidad en redes sociales? 10 claves para maximizar tus resultados",
-      image_url: "../assets/img/blog/como-hacer-publicidad-en-redes-sociales.jpg",
-      href:"como-hacer-publicidad-en-redes-sociales",
-      alt:"como hacer publicidad en redes sociales 10 claves para maximizar tus resultados"
-    }
-  ];
+  {
+    title: "¿Cómo ganar nuevos clientes a través del SEO?",
+    image_url:
+      "../assets/img/blog/como-ganar-nuevos-clientes-a-traves-del-seo.jpg",
+    href: "como-ganar-nuevos-clientes-a-traves-del-seo",
+    alt: "como ganar nuevos clientes a traves del seo",
+  },
+  {
+    title: "Qué son las palabras clave y cómo usarlas",
+    image_url:
+      "../assets/img/blog/que-son-las-palabras-clave-y-como-usarlas.jpg",
+    href: "que-son-las-palabras-clave-y-como-usarlas",
+    alt: "que son las palabras clave y como usarlas",
+  },
+  {
+    title:
+      "¿Cómo hacer publicidad en redes sociales? 10 claves para maximizar tus resultados",
+    image_url: "../assets/img/blog/como-hacer-publicidad-en-redes-sociales.jpg",
+    href: "como-hacer-publicidad-en-redes-sociales",
+    alt: "como hacer publicidad en redes sociales 10 claves para maximizar tus resultados",
+  },
+];
 
-  const posts2 = [
-    {
-      title: "Claves del diseño web profesional",
-      image_url: "../assets/img/blog/claves-del-diseno-web-profesional.jpg",
-      href:"claves-del-diseno-web-profesional",
-      alt:"claves del disenio web profesional"
-    },
-    {
-      title: "Como Promocionar tu empresa en las fiestas",
-      image_url: "../assets/img/blog/como-promocionar-tu-empresa-en-las-fiestas.jpeg",
-      href:"como-promocionar-tu-empresa-en-las-fiestas",
-      alt:"como promocionar tu empresa en la fiestas"
-    },
-    {
-      title: "¿E-Commerce: ¿Cómo crear una tienda online?",
-      image_url: "../assets/img/blog/e-commerce-como-crear-una-tienda-online.jpeg",
-      href:"como-crear-una-tienda-online",
-      alt:"como crear una tienda online"
-    }
-  ];
+const posts2 = [
+  {
+    title: "Claves del diseño web profesional",
+    image_url: "../assets/img/blog/claves-del-diseno-web-profesional.jpg",
+    href: "claves-del-diseno-web-profesional",
+    alt: "claves del disenio web profesional",
+  },
+  {
+    title: "Como Promocionar tu empresa en las fiestas",
+    image_url:
+      "../assets/img/blog/como-promocionar-tu-empresa-en-las-fiestas.jpeg",
+    href: "como-promocionar-tu-empresa-en-las-fiestas",
+    alt: "como promocionar tu empresa en la fiestas",
+  },
+  {
+    title: "¿E-Commerce: ¿Cómo crear una tienda online?",
+    image_url:
+      "../assets/img/blog/e-commerce-como-crear-una-tienda-online.jpeg",
+    href: "como-crear-una-tienda-online",
+    alt: "como crear una tienda online",
+  },
+];
 
-  const blogPostsContainer = document.getElementById("blog-posts");
-  if(blogPostsContainer){
-    posts.forEach((post) => {
-      const postElement = document.createElement("div");
-      postElement.className = "col-lg-4 col-md-4 mx-1";
-      postElement.id="PostCard";
-      postElement.innerHTML = `
+const blogPostsContainer = document.getElementById("blog-posts");
+if (blogPostsContainer) {
+  posts.forEach((post) => {
+    const postElement = document.createElement("div");
+    postElement.className = "col-lg-4 col-md-4 mx-1";
+    postElement.id = "PostCard";
+    postElement.innerHTML = `
       <div class="single-blog mx-3">
         <div class="single-blog-img p-3">
           <a href="${post.href}"><img src="${post.image_url}" alt="${post.alt}" /></a>
@@ -60,17 +65,17 @@ const posts = [
         <p>&nbsp;</p>
       </div>
       `;
-      blogPostsContainer.appendChild(postElement);
-    });
-  }
+    blogPostsContainer.appendChild(postElement);
+  });
+}
 
-  const blogPostsContainer2 = document.getElementById("blog-posts2");
-  if(blogPostsContainer2){
-    posts2.forEach((post) => {
-      const postElement = document.createElement("div");
-      postElement.className = "col-lg-4 col-md-4 mx-1";
-      postElement.id="PostCard";
-      postElement.innerHTML = `
+const blogPostsContainer2 = document.getElementById("blog-posts2");
+if (blogPostsContainer2) {
+  posts2.forEach((post) => {
+    const postElement = document.createElement("div");
+    postElement.className = "col-lg-4 col-md-4 mx-1";
+    postElement.id = "PostCard";
+    postElement.innerHTML = `
       <div class="single-blog mx-3">
         <div class="single-blog-img p-3">
           <a href="${post.href}"><img src="${post.image_url}" alt="${post.alt}" /></a>
@@ -84,7 +89,6 @@ const posts = [
          <p>&nbsp;</p>
       </div>
       `;
-      blogPostsContainer2.appendChild(postElement);
-    });
-  }
-  
+    blogPostsContainer2.appendChild(postElement);
+  });
+}
--- assets/js/progress-bar.js
+++ assets/js/progress-bar.js
@@ -1,8 +1,1 @@
-document.addEventListener("scroll", () => {
-    const scrollTop = window.scrollY;
-    const documentHeight = document.documentElement.scrollHeight;
-    const windowHeight = window.innerHeight;
-    const scrollPercentage = (scrollTop / (documentHeight - windowHeight)) * 100;
-    const progressBar = document.querySelector(".progress-bar");
-    progressBar.style.width = `${scrollPercentage}%`;
-  });
+document.addEventListener("scroll",()=>{const scrollTop=window.scrollY;const documentHeight=document.documentElement.scrollHeight;const windowHeight=window.innerHeight;const scrollPercentage=(scrollTop/(documentHeight-windowHeight))*100;const progressBar=document.querySelector(".progress-bar");progressBar.style.width=`${scrollPercentage}%`})
