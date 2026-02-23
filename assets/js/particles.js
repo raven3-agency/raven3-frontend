1231dd216ab7c0d8fcd6ade560f5e7c2d79ee7c4
Patricio Monpelat <patricio.monpelat@gmail.com>
2026-02-23 14:04:54 -0300

    .

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1,6 +1,3 @@
-import * as THREE from "three";
-// si necesitás OrbitControls u otros:
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 // assets/js/particles.js
 (() => {
   function start() {
