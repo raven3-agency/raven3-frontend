64ad28e153bc76e55e65b78fb6a14e3c31293601
Patricio Monpelat <patricio.monpelat@gmail.com>
2026-02-27 20:50:25 -0300

    delete old web

--- OLDWEB/assets/js/progress-bar.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("scroll",()=>{const scrollTop=window.scrollY;const documentHeight=document.documentElement.scrollHeight;const windowHeight=window.innerHeight;const scrollPercentage=(scrollTop/(documentHeight-windowHeight))*100;const progressBar=document.querySelector(".progress-bar");progressBar.style.width=`${scrollPercentage}%`})
--- OLDWEB/assets/js/theme.js
+++
@@ -1,3 +0,0 @@
-function theme_set(toggled){document.getElementById("style-light").disabled=toggled;document.getElementById("style-dark").disabled=!toggled;localStorage.setItem("theme-toggled",toggled?"1":"")}
-function theme_toggle(){theme_set(!document.getElementById("style-light").disabled)}
-theme_set(localStorage.getItem("theme-toggled"));let preloader=document.getElementById("preloader");if(preloader){window.addEventListener("load",()=>{preloader.remove()})}
