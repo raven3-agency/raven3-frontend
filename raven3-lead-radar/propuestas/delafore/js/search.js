3f52ce63b87c17e424811f8abf2980eb39a78709
Patricio Monpelat <patricio.monpelat@gmail.com>
2026-06-12 18:30:51 -0300

    .

--- raven3-lead-radar/propuestas/delafore/js/search.js
+++ raven3-lead-radar/propuestas/delafore/js/search.js
@@ -197,13 +197,6 @@
           <!-- Badge destacada -->
           ${destacadaBadge}
 
-          <!-- Botón favorito -->
-          <button class="fav-btn" data-id="${p.id}" aria-label="Guardar en favoritos" title="Guardar en favoritos">
-            <svg width="15" height="15" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"/>
-            </svg>
-          </button>
-
           <!-- Flechas del slider -->
           ${arrowsHTML}
 
